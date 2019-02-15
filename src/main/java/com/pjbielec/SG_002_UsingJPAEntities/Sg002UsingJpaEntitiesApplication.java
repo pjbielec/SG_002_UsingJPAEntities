773bcf8fc4a4beb3a4b5267e2109e5598afd1f7b
pjbielec <przemyslaw.bielec@core3.pl>
2019-02-15 11:58:56 +0100

    Initial commit

---
+++ src/main/java/com/pjbielec/SG_002_UsingJPAEntities/Sg002UsingJpaEntitiesApplication.java
@@ -0,0 +1,14 @@
+package com.pjbielec.SG_002_UsingJPAEntities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sg002UsingJpaEntitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sg002UsingJpaEntitiesApplication.class, args);
+	}
+
+}
+
